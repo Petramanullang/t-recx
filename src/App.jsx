c0e21a68afdce092e1061355791fc8932a761262
Petramanullang <petra.mainacc@gmail.com>
2025-11-20 22:07:57 +0700

    Add: make a boilerplate for multipage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import {Routes, Route} from "react-router-dom";
+import Home from "./page/Home";
+import About from "./page/About";
+
+
 function App() {
   return (
-    <>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
